d0f91457888d256850c607ce28dbecada2417929
hushmoon <zjuelma@gmail.com>
2024-09-01 00:42:06 +0800

    LCD1602

--- LCD1602.c
+++ LCD1602.c
@@ -4,33 +4,6 @@
  NATSU NO OWARI
 */
 #include "LCD1602.h"
-#include "delay.h"
-#include <INTRINS.H>
-
-// PIN MAPPING
-sbit RS  = P2 ^ 7;
-sbit RW  = P2 ^ 6;
-sbit EN  = P2 ^ 5;
-sbit DB0 = P0 ^ 0;
-sbit DB1 = P0 ^ 1;
-sbit DB2 = P0 ^ 2;
-sbit DB3 = P0 ^ 3;
-sbit DB4 = P0 ^ 4;
-sbit DB5 = P0 ^ 5;
-sbit DB6 = P0 ^ 6;
-sbit DB7 = P0 ^ 7;
-
-#define uchar    unsigned char
-#define uint     unsigned int
-#define DataPort P0
-
-#define CLR_RS   (RS = 0)
-#define SET_RS   (RS = 1)
-#define CLR_RW   (RW = 0)
-#define SET_RW   (RW = 1)
-#define CLR_EN   (EN = 0)
-#define SET_EN   (EN = 1)
-
 /*
     Name: LCD_Write_Command
     Function: Write command to LCD1602
--- LCD1602.h
+++ LCD1602.h
@@ -1,7 +1,32 @@
-#include <REG52.H>
-#include <INTRINS.H>
-
 #ifndef __LCD1602_H__
 #define __LCD1602_H__
+
+#include <REG52.H>
+#include <INTRINS.H>
+#include "delay.h"
+
+// PIN MAPPING
+sbit RS  = P2 ^ 7;
+sbit RW  = P2 ^ 6;
+sbit EN  = P2 ^ 5;
+sbit DB0 = P0 ^ 0;
+sbit DB1 = P0 ^ 1;
+sbit DB2 = P0 ^ 2;
+sbit DB3 = P0 ^ 3;
+sbit DB4 = P0 ^ 4;
+sbit DB5 = P0 ^ 5;
+sbit DB6 = P0 ^ 6;
+sbit DB7 = P0 ^ 7;
+
+#define uchar    unsigned char
+#define uint     unsigned int
+#define DataPort P0
+
+#define CLR_RS   (RS = 0)
+#define SET_RS   (RS = 1)
+#define CLR_RW   (RW = 0)
+#define SET_RW   (RW = 1)
+#define CLR_EN   (EN = 0)
+#define SET_EN   (EN = 1)
 
 #endif
