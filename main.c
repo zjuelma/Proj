570c7bb472f528211259ec12c1892a6cc6ddf81c
hushmoon <zjuelma@gmail.com>
2024-09-03 14:05:00 +0800

    Preliminary_3

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <REG52.H>
 #include "delay.h"
+#include "LCD1602.h"
+#include "MLX90614.h"
+#include "main.h"
 
 void main(void)
 {
     DelayMs(500);
+    LCD_Init();
     while (1) {
+        Read_T();
+        Display_Temperature();
+        DelayMs(50);
     }
 }
